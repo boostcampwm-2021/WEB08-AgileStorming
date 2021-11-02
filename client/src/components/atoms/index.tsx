c75436803f8b49426851de8a803269ff88dce832
chosh <chotjd329@hotmail.com>
2021-11-02 21:44:23 +0900

    ✨ BoxButton atom 추가 #15
    버튼 atom 추가
    onClick 이벤트 핸들러 필수로 받도록 설정
    margin과 color, bgcolor 스타일 인자로 받도록 설정

--- client/src/components/atoms/index.tsx
+++ client/src/components/atoms/index.tsx
@@ -1,1 +1,3 @@
+export { default as BoxButton } from 'components/atoms/BoxButton';
 export { default as ShareButton } from 'components/atoms/ShareButton';
+export { default as Title } from 'components/atoms/Title';
