f0195d17423befd639404b6d0a60bea123ffc076
chosh <chotjd329@hotmail.com>
2021-11-02 20:09:55 +0900

    ✨ 로그인 페이지에 타이틀 설명 추가
    Title 컴포넌트 활용해서 안내 문구 표시

---
+++ client/src/components/templates/Login/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { common } from 'styles'
+import { Template } from "./style"
+import Title from "components/atoms/Title"
+
+const Login = () => {
+  return (
+    <Template>
+      <Title size={common.fontSize.title} toLeft={true}>쉽게 쓰는 실시간 애자일 협업툴</Title>
+      <Title size={common.fontSize.xxxlarge} toLeft={true}>마인드맵으로 동료들과 작업을 간편하게 관리할 수 있습니다.</Title>
+    </Template >
+  )
+}
+
+export default Login
