f76918ddab3916c4f6dece80544d66c3c36da7b6
chosh <chotjd329@hotmail.com>
2021-11-02 22:33:49 +0900

    ⚡️ Title 컴포넌트 line height 개선
    default margin을 줄이고 line height를 늘림으로서 글이 넘어갔을때에도 보기좋게 대응하도록 수정

--- client/src/components/atoms/Title/index.tsx
+++ client/src/components/atoms/Title/index.tsx
@@ -8,9 +8,10 @@ interface IProps {
 }
 
 export const Title = styled.div`
-margin: ${(props: IProps) => props.margin ?? "1rem"};
+margin: ${(props: IProps) => props.margin ?? "0.5rem"};
 margin-left: ${(props: IProps) => props.toLeft ? "auto" : ""};
 font-size: ${(props: IProps) => props.size ?? common.fontSize.normal};
 font-weight: bold;
+line-height: 1.2;
 `
 export default Title
