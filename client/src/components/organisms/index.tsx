ff05296bd93b7b53ae9e724bc03d597e0bb92189
juyahn <juyahn213@gmail.com>
2021-11-03 15:15:34 +0900

    ✨새 프로젝트 생성 버튼 #11
    ProjectCard와 겉보기에 비슷해서 Card라 네이밍 했지만 버튼으로 바꾸는게 나을 수도 있음. 모달창 띄우는 이벤트 작업 필요

--- client/src/components/organisms/index.tsx
+++ client/src/components/organisms/index.tsx
@@ -1,2 +1,4 @@
-export { default as ProjectCardContainer } from 'components/organisms/ProjectCardContainer';
+export { default as ProjectCardContainer } from 'components/templates/ProjectCardContainer';
 export { default as LoginWrapper } from 'components/organisms/LoginWrapper';
+export { default as NewProjectCard } from 'components/organisms/NewProjectCard';
+export { default as ProjectCard } from 'components/organisms/ProjectCard';
