b80879a98e5eaf559d1a973a63d4a0da6b5491c9
juyahn <juyahn213@gmail.com>
2021-11-03 14:36:51 +0900

    🐛 ShareButton Atom IconButton으로 변경
    공유, 삭제, 추가 등 이미지 버튼이 여러 개 쓰일 것 같다고 예상되어 img src와 click handler를 받는 버튼으로 수정. img와 button으로 atom 분리 후 단계 업그레이드 고려 중

--- client/src/components/atoms/index.tsx
+++ client/src/components/atoms/index.tsx
@@ -1,5 +1,5 @@
 export { default as BoxButton } from 'components/atoms/BoxButton';
 export { default as Input } from 'components/atoms/Input';
-export { default as ShareButton } from 'components/atoms/ShareButton';
+export { default as IconButton } from 'components/atoms/IconButton';
 export { default as Toast } from 'components/atoms/Toast';
 export { default as Title } from 'components/atoms/Title';
