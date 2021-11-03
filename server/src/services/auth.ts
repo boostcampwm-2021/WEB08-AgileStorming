d2fc6b5c9a1f2a1752411bcdf09f3cf1ba377bd8
chosh <chotjd329@hotmail.com>
2021-11-03 15:36:46 +0900

    ✨ jwt 생성함수 추가 #3
    아이디를 입력받아 jwt를 생성해주는 service 함수 추가

---
+++ server/src/services/auth.ts
@@ -0,0 +1,7 @@
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const createJWTToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+};
