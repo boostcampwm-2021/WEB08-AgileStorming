52a01bcfe9eb7a45f8f2f27932e644a55ac13849
chosh <chotjd329@hotmail.com>
2021-11-03 20:50:49 +0900

    🔧 ormconfig에 seeding 경로 추가 및 db관련 파일 묶기#21
    database에서 db관련 폴더 모으기

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import { ConnectionOptions } from 'typeorm';
 dotenv.config();
 
-const ormConfig: ConnectionOptions = {
+const ormConfig: ConnectionOptionsWithSeed = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
@@ -11,14 +11,21 @@ const ormConfig: ConnectionOptions = {
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
-  entities: ['src/entity/**/*.ts'],
+  entities: ['src/database/entities/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
   cli: {
-    entitiesDir: 'src/entity',
+    entitiesDir: 'src/database/entities',
     migrationsDir: 'src/migration',
     subscribersDir: 'src/subscriber',
   },
+  seeds: ['src/database/seeds/**/*.seed.ts'],
+  factories: ['src/database/factory/**/*.factory.ts'],
+};
+
+type ConnectionOptionsWithSeed = ConnectionOptions & {
+  seeds: string[];
+  factories: string[];
 };
 
 export default ormConfig;
