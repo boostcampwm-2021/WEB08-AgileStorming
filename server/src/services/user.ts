ec28a5b7766440b27b7502aa28ba1863bc5d9d36
chosh <chotjd329@hotmail.com>
2021-11-03 22:07:26 +0900

    ✨ 회원가입 api 추가 #9
    아이디 확인 후, 랜덤으로 아이콘, 컬러 추가

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -1,7 +1,13 @@
-import { getConnection, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
+import { Animal } from '../database/entities/Animal';
+import { Color } from '../database/entities/Color';
 import { User } from '../database/entities/User';
 
-export const findOneUser = (id: string) => {
-  const userRepository = getConnection().getRepository(User);
-  return userRepository.findOne({ where: { id } });
+export const createUser = async (id: string, name: string) => {
+  const animal = await getRepository(Animal).createQueryBuilder().orderBy('RAND()').getOne();
+  const color = await getRepository(Color).createQueryBuilder().orderBy('RAND()').getOne();
+  const newUser = { id, name, icon: animal.name, color: color.rgb };
+  return getRepository(User).createQueryBuilder().insert().values(newUser).execute();
 };
+
+export const findOneUser = (id: string) => getRepository(User).findOne({ where: { id } });
