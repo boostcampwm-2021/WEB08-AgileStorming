b6ebab22e554c8ad9d6680967ee7fad2507209e7
juyahn <juyahn213@gmail.com>
2021-11-03 17:30:07 +0900

    ✨ 모달창, 모달오버레이 atom 정의 #11
    styled로만 정의함. 추후 필요하면 수정

--- client/src/components/atoms/index.tsx
+++ client/src/components/atoms/index.tsx
@@ -4,3 +4,5 @@ export { default as Toast } from 'components/atoms/Toast';
 export { default as Title } from 'components/atoms/Title';
 export { default as IconImg } from 'components/atoms/IconImg';
 export { default as TransparentButton } from 'components/atoms/TransparentButton';
+export { default as ModalBox } from 'components/atoms/ModalBox';
+export { default as ModalOverlay } from 'components/atoms/ModalOverlay';
