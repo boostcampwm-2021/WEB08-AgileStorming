a16bc8582a26d8df56b8bebfe9e2c797427a20c6
juyahn <juyahn213@gmail.com>
2021-11-03 21:50:31 +0900

    💄 TextButton margin props
    입력받아서 적용할 수 있게 함

--- client/src/components/molecules/TextButton/index.tsx
+++ client/src/components/molecules/TextButton/index.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { SmallText, TransparentButton } from 'components/atoms';
 import * as Types from 'styles/common';
+import styled from '@emotion/styled';
 
 interface IProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   text: string;
   textColor: Types.TColor;
   textWeight?: string;
+  margin: string;
 }
 
-const TextButton: React.FC<IProps> = ({ onClick, text, textColor, textWeight }) => {
+interface IStyleProps {
+  margin: string;
+}
+
+const StyledTextButton = styled.div<IStyleProps>`
+  width: fit-content;
+  height: fit-content;
+  margin: ${(props) => props.margin};
+`;
+
+const TextButton: React.FC<IProps> = ({ onClick, text, textColor, textWeight, margin }) => {
   return (
-    <TransparentButton onClick={onClick}>
-      <SmallText color={textColor} weight={textWeight}>
-        {text}
-      </SmallText>
-    </TransparentButton>
+    <StyledTextButton margin={margin}>
+      <TransparentButton onClick={onClick}>
+        <SmallText color={textColor} weight={textWeight}>
+          {text}
+        </SmallText>
+      </TransparentButton>
+    </StyledTextButton>
   );
 };
 
