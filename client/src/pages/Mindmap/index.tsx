e3cb29c96ff2457f6ca62977f4f719356ffc67a7
SeungHyun Kim <daejjyu@gmail.com>
2021-11-04 18:36:59 +0900

    ♻️ 더미 코드 리뷰 반영하여 recoil부분으로 이동 #10
    성현님 리뷰 반영

--- client/src/pages/Mindmap/index.tsx
+++ client/src/pages/Mindmap/index.tsx
@@ -1,16 +1,10 @@
 import { MindmapBackground, MindmapTree } from 'components/molecules';
-<<<<<<< HEAD
 import MindmapWrapper from 'components/organisms/MindmapBtnWrapper';
 import { useRecoilValue } from 'recoil';
-=======
-// import { useRecoilValue } from 'recoil';
->>>>>>> 2364753 (✨ 헤더바 페이지 별 분기처리 없이 라우터에 단순 구현)
-// import { mindMapState } from 'recoil/mindMap';
-import { IMindMap } from 'recoil/mindMap';
+import { mindMapState } from 'recoil/mindMap';
 
 const Mindmap = () => {
-  // const mindMap = useRecoilValue(mindMapState);
-  const mindMap = getDummyMindmap();
+  const mindMap = useRecoilValue(mindMapState);
   return (
     <>
       <MindmapBackground>
@@ -19,31 +13,6 @@ const Mindmap = () => {
       </MindmapBackground>
     </>
   );
-};
-
-///더미코드 삭제 예정
-const getDummyMindmap = (): IMindMap => {
-  const mindNodes = new Map();
-  Array(10)
-    .fill(0)
-    .map((v, i) => ({ nodeId: i + 10, level: 'TASK', content: 'TASK', children: [] }))
-    .forEach((v) => mindNodes.set(v.nodeId, v));
-  [
-    { nodeId: 9, level: 'STORY', content: 'STORY', children: [10, 11] },
-    { nodeId: 8, level: 'STORY', content: 'STORY', children: [12, 19] },
-    { nodeId: 7, level: 'STORY', content: 'STORY', children: [] },
-    { nodeId: 6, level: 'STORY', content: 'STORY', children: [13, 14, 15] },
-    { nodeId: 5, level: 'STORY', content: 'STORY', children: [16] },
-    { nodeId: 4, level: 'STORY', content: 'STORY', children: [17, 18] },
-    { nodeId: 3, level: 'EPIC', content: 'EPIC', children: [] },
-    { nodeId: 2, level: 'EPIC', content: 'EPIC', children: [4, 5, 6, 7] },
-    { nodeId: 1, level: 'EPIC', content: 'EPIC', children: [8, 9] },
-    { nodeId: 0, level: 'ROOT', content: 'ROOT', children: [1, 2, 3] },
-  ].forEach((v) => mindNodes.set(v.nodeId, v));
-  return {
-    rootId: 0,
-    mindNodes: mindNodes,
-  };
 };
 
 export default Mindmap;
--- client/src/recoil/mindMap/index.ts
+++ client/src/recoil/mindMap/index.ts
@@ -2,6 +2,31 @@ import { atom } from 'recoil';
 
 export type levels = 'ROOT' | 'EPIC' | 'STORY' | 'TASK';
 
+///더미코드 삭제 예정
+const getDummyMindmap = (): IMindMap => {
+  const mindNodes = new Map();
+  Array(10)
+    .fill(0)
+    .map((v, i) => ({ nodeId: i + 10, level: 'TASK', content: 'TASK', children: [] }))
+    .forEach((v) => mindNodes.set(v.nodeId, v));
+  [
+    { nodeId: 9, level: 'STORY', content: 'STORY', children: [10, 11] },
+    { nodeId: 8, level: 'STORY', content: 'STORY', children: [12, 19] },
+    { nodeId: 7, level: 'STORY', content: 'STORY', children: [] },
+    { nodeId: 6, level: 'STORY', content: 'STORY', children: [13, 14, 15] },
+    { nodeId: 5, level: 'STORY', content: 'STORY', children: [16] },
+    { nodeId: 4, level: 'STORY', content: 'STORY', children: [17, 18] },
+    { nodeId: 3, level: 'EPIC', content: 'EPIC', children: [] },
+    { nodeId: 2, level: 'EPIC', content: 'EPIC', children: [4, 5, 6, 7] },
+    { nodeId: 1, level: 'EPIC', content: 'EPIC', children: [8, 9] },
+    { nodeId: 0, level: 'ROOT', content: 'ROOT', children: [1, 2, 3] },
+  ].forEach((v) => mindNodes.set(v.nodeId, v));
+  return {
+    rootId: 0,
+    mindNodes: mindNodes,
+  };
+};
+
 export interface IMindNode {
   nodeId: number;
   level: levels;
@@ -14,15 +39,16 @@ export interface IMindMap {
   mindNodes: Map<number, IMindNode>;
 }
 
-const initRootId = 0;
-const initRootNode = {
-  nodeId: initRootId,
-  level: 'ROOT' as levels,
-  content: '',
-  children: [],
-};
+// const initRootId = 0;
+// const initRootNode = {
+//   nodeId: initRootId,
+//   level: 'ROOT' as levels,
+//   content: '',
+//   children: [],
+// };
 
 export const mindMapState = atom<IMindMap>({
   key: 'mindMapAtom',
-  default: { rootId: initRootId, mindNodes: new Map([[initRootId, initRootNode]]) },
+  // default: { rootId: initRootId, mindNodes: new Map([[initRootId, initRootNode]]) },
+  default: getDummyMindmap(),
 });
