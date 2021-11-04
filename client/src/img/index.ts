a0c024cabcbe98d2741a7593fadc908ee9a76bd3
yusokk <13chakbo@naver.com>
2021-11-04 16:11:21 +0900

    ✨ mindmap에 hitsory 버튼, 노드 추가 버튼 UI 추가

--- client/src/img/index.ts
+++ client/src/img/index.ts
@@ -1,7 +1,11 @@
 import sharePath from './share.svg';
 import logoPath from './logo.png';
 import plusPath from './plus.svg';
+import clockPath from './clock.svg';
+import plusCirclePath from './plusCircle.svg';
 
 export const share: string = sharePath;
 export const logo: string = logoPath;
 export const plus: string = plusPath;
+export const clock: string = clockPath;
+export const plusCircle: string = plusCirclePath;
