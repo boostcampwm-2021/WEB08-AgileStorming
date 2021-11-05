99888cb52895927c1422f3085fead5967d790988
yusokk <13chakbo@naver.com>
2021-11-06 02:40:25 +0900

    🚧 마인드맵 노드 선을 그려주기 위한 svg 영역 설정 #12
    - 노드의 상대적인 좌표를 이용해 svg 태그가 삽입될 위치 설정
    - 아직 선을 그리지는 못함

--- client/src/components/molecules/MindmapTree/index.tsx
+++ client/src/components/molecules/MindmapTree/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import { IMindNode, IMindMap } from 'recoil/mindMap';
 import Node from 'components/atoms/Node';
@@ -11,9 +11,34 @@ interface IStyleProps {
   isRoot: boolean;
 }
 
+interface ICoord {
+  x: number;
+  y: number;
+}
+
+interface IRect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  type: QUATER_SPACE;
+}
+
+interface IGap {
+  leftGap: number;
+  topGap: number;
+}
+
+enum QUATER_SPACE {
+  FIRST = 1,
+  SECOND,
+  THIRD,
+  FOURTH,
+}
+
 const NodeContainer = styled.div<IStyleProps>`
+  ${(props) => (props.isRoot ? props.theme.absoluteCenter : { position: 'relative' })};
   ${(props) => props.theme.flex.center};
-  ${(props) => props.isRoot && props.theme.absoluteCenter};
   gap: 1rem;
   border: 1px solid blue;
 `;
@@ -23,16 +48,91 @@ const ChildContainer = styled.div`
   gap: 1rem;
 `;
 
-const getNodeContainer = (nodeId: number, mindNodes: Map<number, IMindNode>) => {
+const calcRect = (parentCoord: ICoord, childCoord: ICoord, gap: IGap, type: QUATER_SPACE): IRect => ({
+  x: childCoord.x - Math.abs(childCoord.x + gap.leftGap - parentCoord.x),
+  y: type === 1 ? childCoord.y : childCoord.y - Math.abs(childCoord.y + gap.topGap - parentCoord.y),
+  width: Math.abs(childCoord.x + gap.leftGap - parentCoord.x),
+  height: Math.abs(childCoord.y + gap.topGap - parentCoord.y),
+  type: type,
+});
+
+const GetNodeContainer = (nodeId: number, mindNodes: Map<number, IMindNode>, parentCoord: ICoord | null) => {
   const isRoot = nodeId === 0;
   const node = mindNodes.get(nodeId);
   const { level, content, children } = node!;
+
+  const [coord, setCoord] = useState<ICoord | null>(null);
+  const [rect, setRect] = useState<IRect | null>(null);
+  const nodeRef = useRef(null);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!nodeRef.current || !containerRef.current) return;
+
+    const currentNode: HTMLElement = nodeRef.current;
+    const currentContainer: HTMLElement = containerRef.current;
+
+    const currentRelativeCoord = {
+      x: Math.floor(currentNode.offsetLeft + currentNode.offsetWidth / 2),
+      y: Math.floor(currentNode.offsetTop + currentNode.offsetHeight / 2),
+    };
+    const currentGap = {
+      topGap: Math.floor(currentContainer.offsetTop),
+      leftGap: Math.floor(currentContainer.offsetLeft),
+    };
+
+    setCoord(currentRelativeCoord);
+
+    if (!parentCoord || !currentRelativeCoord || !currentGap) return;
+    const type = currentRelativeCoord.y + currentGap.topGap > parentCoord.y ? QUATER_SPACE.FOURTH : QUATER_SPACE.FIRST;
+    setRect(calcRect(parentCoord, currentRelativeCoord, currentGap, type));
+    console.log(
+      rect &&
+        'M' +
+          (rect.type === 1 ? `${rect.x},${rect.y + rect.height}` : `${rect.x},${rect.y}`) +
+          'L' +
+          (rect.type === 1 ? `${rect.x + rect.width},${rect.y}` : `${rect.x + rect.width},${rect.y + rect.height}`)
+    );
+  }, [parentCoord]);
+
   return (
-    <NodeContainer key={nodeId} isRoot={isRoot} draggable='true'>
-      <Node id={nodeId.toString()} level={level}>
+    <NodeContainer ref={containerRef} key={nodeId} isRoot={isRoot} draggable='true'>
+      <Node ref={nodeRef} id={nodeId.toString()} level={level}>
         {content}
       </Node>
-      <ChildContainer>{children.map((childrenId) => getNodeContainer(childrenId, mindNodes))}</ChildContainer>
+      {parentCoord && coord && rect ? (
+        <svg
+          width={rect.width}
+          height={rect.height}
+          xmlns='http://www.w3.org/2000/svg'
+          xmlnsXlink='http://www.w3.org/1999/xlink'
+          style={{
+            position: 'absolute',
+            strokeWidth: '0px',
+            backgroundColor: 'blue',
+            opacity: '20%',
+            left: rect.x,
+            top: rect.y,
+          }}
+        >
+          {/* <path
+            fill='none'
+            stroke='#000'
+            d={
+              'M' +
+              (rect.type === 1 ? `${rect.x},${rect.y + rect.height}` : `${rect.x},${rect.y}`) +
+              'L' +
+              (rect.type === 1 ? `${rect.x + rect.width},${rect.y}` : `${rect.x + rect.width},${rect.y + rect.height}`)
+            }
+            strokeWidth='0.5'
+            strokeLinecap='round'
+            style={{ position: 'absolute' }}
+          ></path> */}
+        </svg>
+      ) : (
+        ''
+      )}
+      <ChildContainer>{children.map((childrenId) => GetNodeContainer(childrenId, mindNodes, coord))}</ChildContainer>
     </NodeContainer>
   );
 };
@@ -40,7 +140,7 @@ const getNodeContainer = (nodeId: number, mindNodes: Map<number, IMindNode>) => 
 const MindMapTree: React.FC<IProps> = ({ mindMap }) => {
   const { rootId, mindNodes } = mindMap;
 
-  return getNodeContainer(rootId, mindNodes);
+  return GetNodeContainer(rootId, mindNodes, null);
 };
 
 export default MindMapTree;
