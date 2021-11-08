59fe88246ac62bdbd7973e0fcfea6729e2156f46
yusokk <13chakbo@naver.com>
2021-11-08 16:25:27 +0900

    ♻️ MindmapBtnWrapper 리팩토링
    -styled 내 props 비구조화 할당으로 변경
    - useHistoryBtnClick 커스텀 훅

--- client/src/components/organisms/MindmapBtnWrapper/index.tsx
+++ client/src/components/organisms/MindmapBtnWrapper/index.tsx
@@ -5,32 +5,37 @@ import { clock, plusCircle } from 'img';
 import common from 'styles/common';
 
 const Wrapper = styled.div`
-  ${(props) => props.theme.flex.row}
+  ${({ theme }) => theme.flex.row}
   align-items: center;
-  /* justify-content: space-between; */
   position: fixed;
   bottom: 50px;
   left: 50px;
-  /* width: 200px; */
   > *:not(:last-child) {
     margin-right: 10px;
   }
 `;
 
-const MindmapWrapper = () => {
+const useHistoryBtnClick = () => {
   const hitory = useHistory();
+
   const handleHistoryBtnClick = () => {
     hitory.push('/history');
   };
+
+  return handleHistoryBtnClick;
+};
+
+const MindmapWrapper = () => {
+  const handleHistoryBtnClick = useHistoryBtnClick();
   const handlePlusNodeBtnClick = () => {};
 
   return (
     <Wrapper>
       <BoxButton onClick={handleHistoryBtnClick} color={common.color.white}>
-        <img src={clock} alt='히스토리 버튼'></img>
+        <img src={clock} alt='히스토리 버튼' />
       </BoxButton>
       <BoxButton onClick={handlePlusNodeBtnClick} color={common.color.white}>
-        <img src={plusCircle} alt='노드 추가 버튼'></img>
+        <img src={plusCircle} alt='노드 추가 버튼' />
         {'노드 추가하기'}
       </BoxButton>
     </Wrapper>
