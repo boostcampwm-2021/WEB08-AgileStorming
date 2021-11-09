785aeb194485423df40623da19addbab9b63cb1c
chosh <chotjd329@hotmail.com>
2021-11-09 18:42:00 +0900

    ✨ Label 컴포넌트 추가 #16
    ratio로 라벨의 비율을 조절할 수 있음

--- client/src/components/molecules/index.tsx
+++ client/src/components/molecules/index.tsx
@@ -1,6 +1,7 @@
 export { default as IconButton } from 'components/molecules/IconButton';
 export { default as TextButton } from 'components/molecules/TextButton';
 export { default as PrimaryButton } from 'components/molecules/PrimaryButton';
+export { default as Label } from 'components/molecules/Label';
 export { default as MindmapBackground } from 'components/molecules/MindmapBackground';
 export { default as PopupItemLayout } from 'components/molecules/PopupItemLayout';
 export { default as PopupLayout } from 'components/molecules/PopupLayout';
