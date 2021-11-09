a1480310c90919fb17f585b05d3742fb927ccdc0
yusokk <13chakbo@naver.com>
2021-11-09 21:42:22 +0900

    ✨ useProjectId 커스텀 훅 제작, url params의 projectId 반환

---
+++ client/src/hooks/useRoomId/index.tsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router';
+
+interface IParams {
+  projectId: string;
+}
+
+const useProjectId = () => {
+  const params = useParams<IParams>();
+  return params.projectId;
+};
