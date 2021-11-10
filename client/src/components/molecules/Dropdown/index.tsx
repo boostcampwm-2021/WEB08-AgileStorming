68a253379ce544dfd7cc694c1de7a59d9b526aef
chosh <chotjd329@hotmail.com>
2021-11-10 14:12:03 +0900

    ✨ 드롭다운 컴포넌트 추가 #71
    ul과 input 태그 활용

---
+++ client/src/components/molecules/Dropdown/index.tsx
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from 'react';
+import { DropdownList, StyledInput, TStyle, Wrapper } from './style';
+
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  onValueChange?: Function;
+  dropdownStyle?: TStyle;
+  items?: string[];
+  margin?: string;
+}
+
+const Dropdown: React.FC<IProps> = ({ onValueChange, dropdownStyle = 'normal', items = [], margin = '0', ...props }) => {
+  const activatorRef = useRef<HTMLInputElement>(null);
+  const listRef = useRef<HTMLUListElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickDropdown = (e: React.MouseEvent) => setIsOpen(!isOpen);
+  const handleClickItem = (value: string) => {
+    if (activatorRef.current) {
+      activatorRef.current.value = value;
+    }
+    if (onValueChange) {
+      onValueChange(value);
+    }
+    setIsOpen(false);
+  };
+  const handleClickOutside = (e: MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (listRef.current!.contains(target) || activatorRef.current!.contains(target)) {
+      return;
+    }
+    setIsOpen(false);
+  };
+
+  React.useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('click', handleClickOutside);
+    } else {
+      window.removeEventListener('click', handleClickOutside);
+    }
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <Wrapper margin={margin}>
+      <StyledInput dropdownStyle={dropdownStyle} onClick={handleClickDropdown} ref={activatorRef} {...props} readOnly={true} />
+      <DropdownList dropdownStyle={dropdownStyle} visible={isOpen} ref={listRef}>
+        {items.map((item, idx) => (
+          <li key={idx} onClick={() => handleClickItem(item)}>
+            {item}
+          </li>
+        ))}
+      </DropdownList>
+    </Wrapper>
+  );
+};
+
+export default Dropdown;
