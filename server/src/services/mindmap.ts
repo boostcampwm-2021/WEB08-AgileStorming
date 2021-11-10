bc8257894577c214e03e653b05424445419af212
juyahn <juyahn213@gmail.com>
2021-11-10 17:29:10 +0900

    🐛 마인드맵 노드 생성,삭제 로직 수정 #59
    child 노드까지 지우거나, parent가 있는 노드의 추가 등을 구현

--- server/src/services/mindmap.ts
+++ server/src/services/mindmap.ts
@@ -17,14 +17,41 @@ export const getMindMap = async (projectId: string) => {
     .where('project.id = :projectId', { projectId })
     .getMany();
 };
-export const createNode = async (projectId: string, nodeInfo: INode) => {
+export const createNode = async (projectId: string, parendId: number | null, nodeInfo: INode) => {
   const project = await findOneProject(projectId);
-  const newNode = { ...nodeInfo, project };
-  return getRepository(Mindmap).save(newNode);
+  const newNode = await getRepository(Mindmap).save({ children: JSON.stringify([]), ...nodeInfo, project });
+  if (parendId) {
+    addNodeToParent(parendId, newNode.id);
+  }
+  return newNode.id;
 };
-export const deleteNode = async (nodeId: number) => {
-  return getRepository(Mindmap).createQueryBuilder().delete().where({ id: nodeId }).execute();
+export const deleteNode = async (parendId: number, nodeId: number) => {
+  deleteNodeFromParent(parendId, nodeId);
+  deleteNodeAndChildren(nodeId);
+  return;
 };
 export const updateNode = async (nodeId: number, newData: INode) => {
   return getRepository(Mindmap).createQueryBuilder().update().set(newData).where('id = :nodeId', { nodeId }).execute();
+};
+const addNodeToParent = async (parendId: number | null, newNodeId: number) => {
+  const parent = await findOneNode(parendId);
+  const children = [...JSON.parse(parent.children), newNodeId];
+  updateNode(parendId, { children: JSON.stringify(children) });
+};
+const deleteNodeFromParent = async (parendId: number | null, nodeId: number) => {
+  const parent = await findOneNode(parendId);
+  const children = JSON.parse(parent.children).filter((childId: number) => childId !== nodeId);
+  updateNode(parendId, { children: JSON.stringify(children) });
+};
+const deleteNodeAndChildren = async (nodeId: number) => {
+  const node = await findOneNode(nodeId);
+  const children = JSON.parse(node.children) as number[];
+  if (children.length > 0) {
+    children.forEach((child) => deleteNodeAndChildren(child));
+  }
+  getRepository(Mindmap).createQueryBuilder().delete().where({ id: nodeId }).execute();
+};
+const findOneNode = (nodeId: number) => {
+  const id = nodeId.toString(10);
+  return getRepository(Mindmap).findOne({ where: { id } });
 };
