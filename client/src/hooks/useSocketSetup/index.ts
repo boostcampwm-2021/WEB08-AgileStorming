5827c8c3395562dcd50c8b345a29a3d22d0171eb
SeungHyun Kim <daejjyu@gmail.com>
2021-11-11 04:46:55 +0900

    ✨ user, history EventReceiver 구현
    해당 훅에서 각 이벤트 처리

--- client/src/hooks/useSocketSetup/index.ts
+++ client/src/hooks/useSocketSetup/index.ts
@@ -1,15 +1,21 @@
 import useProjectId from 'hooks/useRoomId';
+import useHistroyReceiver, { IHistoryReceiver } from 'hooks/useHistoryReceiver';
 import { useEffect } from 'react';
-import { SetterOrUpdater, useRecoilState } from 'recoil';
+import { SetterOrUpdater, useRecoilState, useSetRecoilState } from 'recoil';
+import { getParsedHistory, historyState } from 'recoil/history';
+import { mindmapState } from 'recoil/mindmap';
 import { ISocket, socketState } from 'recoil/socket';
 import io from 'socket.io-client';
+import useUserReceiver, { IUserReceiver } from 'hooks/useUserReceiver';
 
 interface IInitProps {
   projectId: string;
   setSocket: SetterOrUpdater<ISocket>;
+  historyReceiver: IHistoryReceiver;
+  userReceiver: IUserReceiver;
 }
 
-const initSocket = ({ projectId, setSocket }: IInitProps) => {
+const initSocket = ({ projectId, setSocket, historyReceiver, userReceiver }: IInitProps) => {
   window.socket = io(process.env.REACT_APP_SERVER!, {
     query: {
       projectId,
@@ -19,25 +25,33 @@ const initSocket = ({ projectId, setSocket }: IInitProps) => {
 
   const { socket } = window;
 
-  socket.on('joined', (id) => {
-    console.log('joined', id);
+  socket.on('joined', (userId) => {
+    console.log('joined', userId);
+    userReceiver({ userId, type: 'JOIN' });
   });
-  socket.on('left', (id) => {
-    console.log('left', id);
+  socket.on('left', (userId) => {
+    console.log('left', userId);
+    userReceiver({ userId, type: 'LEFT' });
   });
   socket.on('event', (data) => {
     console.log('event', data);
+    const history = getParsedHistory(data);
+    historyReceiver(history);
   });
-  const nodeFrom = '';
-  const nodeTo = '';
-  const dataFrom = '';
-  setTimeout(() => socket!.emit('event', 'ADD_NODE', JSON.stringify({ nodeFrom, nodeTo, dataFrom, dataTo: { content: '123' } })), 1000);
+
+  socket.on('serverEmit', (data) => {
+    console.log('serverEmit');
+  });
   setSocket({ projectId });
 };
 
 const useSocketSetup = () => {
   const newProjectId = useProjectId();
   const [{ projectId }, setSocket] = useRecoilState(socketState);
+  const [mindmap, setMindmap] = useRecoilState(mindmapState);
+  const setHistory = useSetRecoilState(historyState);
+  const historyReceiver = useHistroyReceiver({ mindmap, setMindmap, setHistory });
+  const userReceiver = useUserReceiver();
   useEffect(() => {
     const isNewProject = projectId !== newProjectId;
     if (isNewProject) {
@@ -45,8 +59,8 @@ const useSocketSetup = () => {
         window.socket!.emit('leave', projectId);
         window.socket = null;
       }
-      initSocket({ projectId: newProjectId, setSocket });
+      initSocket({ projectId: newProjectId, setSocket, historyReceiver, userReceiver });
     }
-  }, [newProjectId, projectId, setSocket]);
+  }, [newProjectId, projectId, setSocket, historyReceiver, userReceiver]);
 };
 export default useSocketSetup;
