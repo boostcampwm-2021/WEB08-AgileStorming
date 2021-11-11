8d0b52613d99c280a50aea154657f5361fbba671
chosh <chotjd329@hotmail.com>
2021-11-11 12:29:12 +0900

    ♻️ 로그인페이지 리팩토링 밑 에러 메시지 수정
    불필요한 wrapper로 나눈 molecule 삭제

--- client/src/components/organisms/index.tsx
+++ client/src/components/organisms/index.tsx
@@ -1,5 +1,4 @@
 export { default as ProjectCardContainer } from 'components/templates/ProjectCardContainer';
-export { default as LoginWrapper } from 'components/organisms/LoginWrapper';
 export { default as NewProjectCard } from 'components/organisms/NewProjectCard';
 export { default as NodeDetailWrapper } from 'components/organisms/NodeDetailWrapper';
 export { default as ProjectCard } from 'components/organisms/ProjectCard';
--- client/src/components/templates/Login/style.ts
+++ client/src/components/templates/Login/style.ts
@@ -1,10 +1,16 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Template = styled.div`
   width: 100%;
-  height :100vh;
+  height: 100vh;
   padding: 2rem;
-  color:${(props) => props.theme.color.white};
+  color: ${(props) => props.theme.color.white};
   background-color: ${(props) => props.theme.color.primary1};
   ${(props) => props.theme.flex.columnCenter}
-`
+`;
+
+export const Wrapper = styled.div`
+  ${({ theme }) => theme.absoluteCenter};
+  ${({ theme }) => theme.flex.columnCenter};
+  width: 300px;
+`;
