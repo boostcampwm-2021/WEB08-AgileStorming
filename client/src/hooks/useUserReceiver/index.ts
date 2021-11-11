ea8ba99efe67ea0b0746e7dd0c0ac7fa7a09d166
juyahn <juyahn213@gmail.com>
2021-11-11 16:08:46 +0900

    ✨유저 접속, 방 나감, 새로 접속 등 처리 #57
    recoil에 user-list와 user connection 정의

--- client/src/hooks/useUserReceiver/index.ts
+++ client/src/hooks/useUserReceiver/index.ts
@@ -1,6 +1,11 @@
+import { useSetRecoilState } from 'recoil';
+import { connectedUserState, forceReloadUserList } from 'recoil/user-list';
+import useToast from 'hooks/useToast';
+
+type TUserReceiverType = 'JOIN' | 'LEFT' | 'NEW' | 'INIT';
 export interface IUserRes {
-  type: string;
-  userId: string;
+  type: TUserReceiverType;
+  data: string | string[];
 }
 
 export interface IUserReceiver {
@@ -8,11 +13,35 @@ export interface IUserReceiver {
 }
 
 const useUserReceiver = () => {
-  const useUserReceiver = ({ type, userId }: IUserRes) => {
+  const setConnectedUser = useSetRecoilState(connectedUserState);
+  const forceUserListReload = useSetRecoilState(forceReloadUserList);
+  const { showMessage } = useToast();
+  const useUserReceiver = ({ type, data }: IUserRes) => {
     switch (type) {
       case 'JOIN':
+        setConnectedUser((users) => {
+          const newUsers = { ...users };
+          newUsers[data as string] = true;
+          return { ...newUsers };
+        });
         break;
       case 'LEFT':
+        setConnectedUser((users) => {
+          const newUsers = { ...users };
+          delete newUsers[data as string];
+          return { ...newUsers };
+        });
+        break;
+      case 'NEW':
+        showMessage(`${data} 님이 새로 참여하셨습니다.`);
+        forceUserListReload((reload) => !reload);
+        break;
+      case 'INIT':
+        const connectedUser: Record<string, boolean> = {};
+        (data as string[]).forEach((userId: string) => {
+          connectedUser[userId] = true;
+        });
+        setConnectedUser(connectedUser);
         break;
       default:
         break;
