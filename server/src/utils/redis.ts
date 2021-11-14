0eb6cdc80ad5c37ca7349efd7c143937884b7eab
chosh <chotjd329@hotmail.com>
2021-11-14 16:58:47 +0900

    🐛 도커에서 레디스 포트 문제 해결
    도커에서 레디스 포트 문제 해결

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -1,7 +1,10 @@
 import redis from 'redis';
 
 export const xread = (stream: string, id: string, callback: (str) => void) => {
-  const xreadClient = redis.createClient();
+  const xreadClient = redis.createClient({
+    port: 6379,
+    host: 'redis',
+  });
   xreadClient.xread('BLOCK', 0, 'STREAMS', stream, id, (err, str) => {
     if (err) throw err;
     callback(str);
@@ -9,7 +12,10 @@ export const xread = (stream: string, id: string, callback: (str) => void) => {
   });
 };
 
-const xaddClient = redis.createClient();
+const xaddClient = redis.createClient({
+  port: 6379,
+  host: 'redis',
+});
 export const xadd = ({ stream, args }: { stream: string; args: string[] }) => {
   xaddClient.xadd(stream, '*', ...args, (err, ErrorStream) => {
     if (err) throw err;
