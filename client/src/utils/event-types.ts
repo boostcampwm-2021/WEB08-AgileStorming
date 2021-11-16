7267a176a2e30b30c6800a0839b1ce3c3261c138
SeungHyun Kim <daejjyu@gmail.com>
2021-11-17 08:36:55 +0900

    🏷️ client에 server event-type.ts 적용
    복붙하였습니다

---
+++ client/src/utils/event-types.ts
@@ -0,0 +1,80 @@
+export type TDeleteNodeData = {
+  nodeId: number;
+  content: string;
+  index: number;
+  status?: string;
+  posX?: string;
+  posY?: string;
+  assignee?: number;
+  priority?: string;
+};
+export type TAddNodeData = {
+  content: string;
+};
+export type TMoveNodeData = {
+  posX?: string;
+  posY?: string;
+};
+export type TUpdateNodeParent = {
+  nodeId: number;
+  nodeParentType: 'Project' | 'Epic' | 'Story';
+};
+export type TUpdateNodeSibling = {
+  parentId: number;
+  children: number[];
+};
+export type TUpdateNodeContent = {
+  content: string;
+};
+export type TTask = {
+  assignee?: number;
+  labels?: number[];
+  priority?: string;
+  dueDate?: string;
+  estimatedTime?: string;
+  finishedTime?: string;
+  sprint?: number;
+};
+export type TUpdateTaskInformation = {
+  changed: TTask;
+};
+export type TAddSprint = {
+  name: string;
+  startDate: string;
+  endDate: string;
+};
+export type TAddLabel = {
+  name: string;
+};
+export type TAddComment = {
+  nodeId: number;
+  comment: string;
+};
+export type TDeleteSprint = {
+  sprintId: number;
+};
+export type TDeleteLabel = {
+  labelId: number;
+};
+export type TDeleteComment = {
+  nodeId: number;
+  commentId: number;
+};
+
+export type THistoryEventType =
+  | 'ADD_NODE'
+  | 'DELETE_NODE'
+  | 'MOVE_NODE'
+  | 'UPDATE_NODE_PARENT'
+  | 'UPDATE_NODE_SIBLING'
+  | 'UPDATE_NODE_CONTENT'
+  | 'UPDATE_TASK_INFORMATION';
+export type THistoryEventData = {
+  nodeFrom?: number | null;
+  nodeTo?: number | null;
+  dataFrom?: TDeleteNodeData | TMoveNodeData | TUpdateNodeParent | TUpdateNodeSibling | TUpdateNodeContent | TUpdateTaskInformation | null;
+  dataTo?: TAddNodeData | TMoveNodeData | TUpdateNodeParent | TUpdateNodeSibling | TUpdateNodeContent | TUpdateTaskInformation | null;
+};
+
+export type TEventType = 'ADD_SPRINT' | 'ADD_LABEL' | 'ADD_COMMENT' | 'DELETE_SPRINT' | 'DELETE_LABEL' | 'DELETE_COMMENT';
+export type TEventData = TAddSprint | TAddLabel | TAddComment | TDeleteSprint | TDeleteLabel | TDeleteComment;
