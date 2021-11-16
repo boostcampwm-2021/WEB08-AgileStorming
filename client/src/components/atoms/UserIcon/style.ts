b68ebd130d7a5ce4b04b0b9e8f7b59b8d5d0a1b5
yusokk <13chakbo@naver.com>
2021-11-16 22:20:32 +0900

    ⚡️ UserIcon에 cursor 옵션 추가

--- client/src/components/atoms/UserIcon/style.ts
+++ client/src/components/atoms/UserIcon/style.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 interface IStyledIcon {
   color: string;
+  cursor?: string;
 }
 
 export const StyledIcon = styled.span<IStyledIcon>`
@@ -9,4 +10,5 @@ export const StyledIcon = styled.span<IStyledIcon>`
   padding: 0.1rem;
   margin: 0.2rem;
   border-radius: 1rem;
+  cursor: ${({ cursor }) => cursor ?? 'default'};
 `;
