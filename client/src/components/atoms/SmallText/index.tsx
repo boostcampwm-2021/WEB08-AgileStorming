a5dda86b7fb6c834fadd1e707f9c43b27888f6ae
chosh <chotjd329@hotmail.com>
2021-11-16 18:56:26 +0900

    ♻️ small text 아톰 리팩토링
    margin 옵션 추가

--- client/src/components/atoms/SmallText/index.tsx
+++ client/src/components/atoms/SmallText/index.tsx
@@ -4,11 +4,13 @@ import * as Types from 'styles/common';
 interface IStyleProps {
   color: Types.TColor;
   weight?: string;
+  margin?: string;
 }
 
 const SmallText = styled.p<IStyleProps>`
-  color: ${(props) => props.theme.color[props.color]};
-  font-size: ${(props) => props.theme.fontSize.small};
-  font-weight: ${(props) => (props.weight ? props.weight : 'normal')};
+  color: ${({ color }) => color};
+  font-size: ${({ theme }) => theme.fontSize.small};
+  font-weight: ${({ weight }) => (weight ? weight : 'normal')};
+  margin: ${({ margin }) => margin};
 `;
 export default SmallText;
