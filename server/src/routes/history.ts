3126347d35a9a40eff861eb14eb1620dbef68e2a
juyahn <juyahn213@gmail.com>
2021-11-17 15:15:47 +0900

    ✨프로젝트 히스토리 api 제작
    api/history/:projectId 로 접근
    body에 rangeFrom, count 넣으면 범위 특정 가능
    기본 값은 최신 30 개

---
+++ server/src/routes/history.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import { xrevrange } from '../utils';
+const router = Router();
+
+router.get('/:projectId', async (req: Request, res: Response) => {
+  try {
+    const { projectId } = req.params;
+    const { rangeFrom, count } = req.body;
+    const history: Array<string | string[]> = await xrevrange({ projectId, from: rangeFrom ?? '+', to: '-', count: count ?? 30 });
+    res.status(200).send(history);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRouter from './auth';
 import userRouter from './user';
 import projectRouter from './project';
+import historyRouter from './history';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/project', projectRouter);
+router.use('/history', historyRouter);
 
 export default router;
