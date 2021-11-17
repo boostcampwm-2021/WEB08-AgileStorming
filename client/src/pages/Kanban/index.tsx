f71154c020f2d81ff9ed6bc1d5c52e79e383b002
juyahn <juyahn213@gmail.com>
2021-11-18 00:54:32 +0900

    ✨칸반보드 #103
    task 필터링해서 보여줌

--- client/src/pages/Kanban/index.tsx
+++ client/src/pages/Kanban/index.tsx
@@ -1,9 +1,21 @@
 import CommonLayout from 'components/templates/CommonLayout';
+import { TaskCardContainer } from 'components/templates';
+import { Wrapper } from 'components/atoms';
+import { useRecoilValue } from 'recoil';
+import { taskState } from 'recoil/mindmap';
 
 const Kanban = () => {
+  const taskNodes = useRecoilValue(taskState);
+  const toDoTasks = taskNodes.filter((task) => task.status === 'To Do');
+  const inProgressTasks = taskNodes.filter((task) => task.status === 'In Progress');
+  const doneTasks = taskNodes.filter((task) => task.status === 'Done');
   return (
     <CommonLayout>
-      <h1>칸반페이지</h1>
+      <Wrapper flex={'center'}>
+        <TaskCardContainer status={'To Do'} taskList={toDoTasks} />
+        <TaskCardContainer status={'In Progress'} taskList={inProgressTasks} />
+        <TaskCardContainer status={'Done'} taskList={doneTasks} />
+      </Wrapper>
     </CommonLayout>
   );
 };
