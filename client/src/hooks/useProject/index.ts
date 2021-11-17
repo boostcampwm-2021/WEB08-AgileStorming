800832503f17e7ebdf926bc479962f9c29b839ab
chosh <chotjd329@hotmail.com>
2021-11-17 20:22:09 +0900

    ✨ 프로젝트 리코일 selector 에서 atom으로 관리 #91 #92
    셀렉터 set 무한루프 이슈 (스스로를 set 할 수 없다.)

--- client/src/hooks/useProject/index.ts
+++ client/src/hooks/useProject/index.ts
@@ -1,14 +1,30 @@
 import useRoomId from 'hooks/useRoomId';
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { projectIdState } from 'recoil/project';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { labelListState, projectIdState, sprintListState, userListState } from 'recoil/project';
+import { project } from 'utils/api';
 
 const useProject = () => {
-  const setProjectId = useSetRecoilState(projectIdState);
-  const projectId = useRoomId();
+  const [projectId, setProjectId] = useRecoilState(projectIdState);
+  const setSprintList = useSetRecoilState(sprintListState);
+  const setUserList = useSetRecoilState(userListState);
+  const setLabelList = useSetRecoilState(labelListState);
+
+  const roomId = useRoomId();
+
+  const setProjectInfoData = async () => {
+    if (roomId === projectId) {
+      return;
+    }
+    const projectInfo = await project.getInfo(roomId);
+    setProjectId(roomId);
+    setSprintList(projectInfo.sprints);
+    setUserList(projectInfo.users);
+    setLabelList(projectInfo.labels);
+  };
 
   useEffect(() => {
-    setProjectId(projectId);
+    setProjectInfoData();
     return () => {
       setProjectId(null);
     };
--- client/src/recoil/project/index.ts
+++ client/src/recoil/project/index.ts
@@ -1,48 +1,24 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 import { ILabel } from 'types/label';
 import { ISprint } from 'types/sprint';
 import { IUser } from 'types/user';
-import { project } from 'utils/api';
 
 export const projectIdState = atom<string | null>({
   key: 'projectIdAtom',
   default: null,
 });
 
-export const projectInfoState = selector({
-  key: 'projectInfo',
-  get: async ({ get }) => {
-    const projectId = get(projectIdState);
-    if (!projectId) return null;
-
-    const projectInfo = await project.getInfo(projectId);
-    return projectInfo;
-  },
-});
-
-export const sprintListState = selector<ISprint[]>({
+export const sprintListState = atom<ISprint[]>({
   key: 'sprintList',
-  get: ({ get }) => {
-    const projectInfo = get(projectInfoState);
-    if (!projectInfo) return [];
-    return projectInfo.sprints;
-  },
+  default: [],
 });
 
-export const userListState = selector<IUser[]>({
+export const userListState = atom<IUser[]>({
   key: 'userList',
-  get: ({ get }) => {
-    const projectInfo = get(projectInfoState);
-    if (!projectInfo) return [];
-    return projectInfo.users;
-  },
+  default: [],
 });
 
-export const labelListState = selector<ILabel[]>({
+export const labelListState = atom<ILabel[]>({
   key: 'labelList',
-  get: ({ get }) => {
-    const projectInfo = get(projectInfoState);
-    if (!projectInfo) return [];
-    return projectInfo.labels;
-  },
+  default: [],
 });
