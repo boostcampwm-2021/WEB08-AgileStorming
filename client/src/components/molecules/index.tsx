dc4042bf7d024d296e177a3302f73ad7fef99664
juyahn <juyahn213@gmail.com>
2021-11-18 11:38:49 +0900

    💄LabelIcon 제작
    테스트는 안해봄

--- client/src/components/molecules/index.tsx
+++ client/src/components/molecules/index.tsx
@@ -13,3 +13,4 @@ export { default as Spinner } from 'components/molecules/Spinner';
 export { default as HistoryWindow } from 'components/molecules/HistoryWindow';
 export { default as TempNode } from 'components/molecules/TempNode';
 export { default as TaskInfo } from 'components/molecules/TaskInfo';
+export { default as LabelIcon } from 'components/molecules/LabelIcon';
