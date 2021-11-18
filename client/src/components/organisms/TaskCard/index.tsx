bb0454f7ef2f00a49df61ecbea0b607b2dacd664
juyahn <juyahn213@gmail.com>
2021-11-18 11:40:57 +0900

    ✨TaskCard
    sprint, label 등 object로 바뀐 거 반영위해 commit

--- client/src/components/organisms/TaskCard/index.tsx
+++ client/src/components/organisms/TaskCard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import { userListState } from 'recoil/project';
+import { labelListState, sprintListState, userListState } from 'recoil/project';
 import { IMindNode } from 'types/mindmap';
 import { Title, SmallText, UserIcon, Wrapper } from 'components/atoms';
-import { TaskInfo } from 'components/molecules';
+import { LabelIcon, TaskInfo } from 'components/molecules';
 import { StyledTaskCard, StyledCardInfoLeft, StyledCardInfoRight } from './style';
 interface IProps {
   taskInfo: IMindNode;
@@ -13,31 +13,30 @@ interface IProps {
 
 const TaskCard: React.FC<IProps> = ({ taskInfo, onDoubleClickTask, onMouseDownTask }) => {
   const userList = useRecoilValue(userListState)!;
+  const sprintList = useRecoilValue(sprintListState)!;
+  const labelList = useRecoilValue(labelListState)!;
   const user = userList.find((users) => users.id === taskInfo.assignee)!;
   return (
     <StyledTaskCard onDoubleClick={onDoubleClickTask} onMouseDown={onMouseDownTask}>
       <Title titleStyle={'normal'}>{taskInfo.content}</Title>
       <Wrapper flex={'row'}>
         <StyledCardInfoLeft>
-          <TaskInfo title={'담당자: '} content={user.name}>
-            <UserIcon user={user} />
+          <TaskInfo title={'담당자: '} content={user ? user.name : ''}>
+            {user !== undefined ?? <UserIcon user={user} />}
           </TaskInfo>
           <TaskInfo title={'중요도: '} content={taskInfo.priority!}></TaskInfo>
-          <TaskInfo
-            title={'라벨: '}
-            content={
-              taskInfo.labels!.reduce((pre, e) => {
-                return pre + ', ' + e;
-              }, '')!
-            }
-          ></TaskInfo>
+          {taskInfo.labels?.map((labelId) => (
+            <LabelIcon key={labelId} label={labelList.find((l) => l.id === labelId)!}></LabelIcon>
+          ))}
         </StyledCardInfoLeft>
         <StyledCardInfoRight>
-          <TaskInfo title={'스프린트: '} content={taskInfo.sprint as unknown as string}></TaskInfo>
-          <TaskInfo title={'예상 소요 시간: '} content={taskInfo.estimatedTime as unknown as string}>
-            <SmallText color={'black'} margin={'0 0 0 3px'}>
-              {'시간 '}
-            </SmallText>
+          <TaskInfo title={'스프린트: '} content={taskInfo.sprint ? sprintList.find((s) => s.id === taskInfo.sprint)!.name : ''}></TaskInfo>
+          <TaskInfo title={'예상 소요 시간: '} content={taskInfo.estimatedTime ?? ''}>
+            {taskInfo.estimatedTime ?? (
+              <SmallText color={'black'} margin={'0 0 0 3px'}>
+                {'시간 '}
+              </SmallText>
+            )}
           </TaskInfo>
           <TaskInfo title={'마감 시간: '} content={taskInfo.dueDate!}></TaskInfo>
         </StyledCardInfoRight>
