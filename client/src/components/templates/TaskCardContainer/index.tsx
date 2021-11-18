bc00fe0c236652e79abaada33ff22d7a6e849f69
juyahn <juyahn213@gmail.com>
2021-11-18 14:03:19 +0900

    ✨task 상태 변경 socket 연결 #105
    drag 함수는 저게 최선이었다..

--- client/src/components/templates/TaskCardContainer/index.tsx
+++ client/src/components/templates/TaskCardContainer/index.tsx
@@ -4,7 +4,8 @@ import { IMindNode } from 'types/mindmap';
 import { TaskCard } from 'components/organisms';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { selectedNodeIdState } from 'recoil/node';
-import { getNextMapState, mindmapState } from 'recoil/mindmap';
+import { mindmapState } from 'recoil/mindmap';
+import useHistoryEmitter from 'hooks/useHistoryEmitter';
 
 const StyledTaskCardContainer = styled.div`
   ${(props) => props.theme.flex.column}
@@ -38,8 +39,8 @@ interface IProps {
 
 const TaskCardContainer: React.FC<IProps> = ({ taskList, status }) => {
   const setSelectedNodeId = useSetRecoilState(selectedNodeIdState);
-  const setMindmapData = useSetRecoilState(mindmapState);
   const { mindNodes } = useRecoilValue(mindmapState);
+  const { updateTaskInformation } = useHistoryEmitter();
   const handleOnDoubleClickTask = (nodeId: number) => {
     setSelectedNodeId(nodeId);
   };
@@ -60,34 +61,39 @@ const TaskCardContainer: React.FC<IProps> = ({ taskList, status }) => {
       clicks = 0;
     }
   };
+  const moveAt = (clone: HTMLElement, pageX: number, pageY: number) => {
+    clone.style.left = pageX - clone.offsetWidth / 2 + 'px';
+    clone.style.top = pageY - clone.offsetHeight / 2 + 'px';
+  };
+  const onMouseMove = (clone: HTMLElement, event: MouseEvent) => {
+    moveAt(clone, event.pageX, event.pageY);
+  };
+  const onMouseUp = (clone: HTMLElement, nodeId: number, mouseMove: (event: MouseEvent) => void, event: MouseEvent) => {
+    clone.style.display = 'none';
+    const elemBelow = document.elementFromPoint(event.clientX, event.clientY)! as HTMLElement;
+    const container = elemBelow.closest('.CardContainer')! as HTMLDivElement;
+    const currentTask = mindNodes.get(nodeId)!;
+    if (currentTask.status !== container.dataset.status) {
+      updateTaskInformation({
+        nodeFrom: nodeId,
+        nodeTo: nodeId,
+        dataFrom: { changed: { status: currentTask.status } },
+        dataTo: { changed: { status: container.dataset.status as 'To Do' | 'In Progress' | 'Done' } },
+      });
+    }
+    document.removeEventListener('mousemove', mouseMove);
+    clone.remove();
+  };
   const drag = (e: React.MouseEvent<HTMLElement>, currentTarget: HTMLElement, nodeId: number) => {
     if (e.buttons !== 1) return;
     const clone = currentTarget.cloneNode(true) as HTMLElement;
     clone.style.position = 'absolute';
     clone.style.zIndex = '1000';
     document.body.append(clone);
-    const moveAt = (pageX: number, pageY: number) => {
-      clone.style.left = pageX - clone.offsetWidth / 2 + 'px';
-      clone.style.top = pageY - clone.offsetHeight / 2 + 'px';
-    };
-    const onMouseMove = (event: MouseEvent) => {
-      moveAt(event.pageX, event.pageY);
-    };
-    moveAt(e.pageX, e.pageY);
-    document.addEventListener('mousemove', onMouseMove);
-    clone.onmouseup = (event: MouseEvent) => {
-      clone.style.display = 'none';
-      const elemBelow = document.elementFromPoint(event.clientX, event.clientY)! as HTMLElement;
-      const container = elemBelow.closest('.CardContainer')! as HTMLDivElement;
-      const currentTask = mindNodes.get(nodeId)!;
-      if (currentTask.status !== container.dataset.status) {
-        mindNodes.set(nodeId, { ...currentTask, status: container.dataset.status as 'To Do' | 'In Progress' | 'Done' });
-        const newMapData = getNextMapState({ rootId: 0, mindNodes });
-        setMindmapData(newMapData);
-      }
-      document.removeEventListener('mousemove', onMouseMove);
-      clone.remove();
-    };
+    moveAt(clone, e.pageX, e.pageY);
+    const mouseMove = (event: MouseEvent) => onMouseMove(clone, event);
+    document.addEventListener('mousemove', mouseMove);
+    clone.addEventListener('mouseup', (event: MouseEvent) => onMouseUp(clone, nodeId, mouseMove, event));
   };
 
   return (
