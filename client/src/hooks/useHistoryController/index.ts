34fb8dffae559b0c0707630e62474ed8a4d4e46b
yusokk <13chakbo@naver.com>
2021-11-21 21:11:45 +0900

    ♻️ historyController 리팩토링
    - 중복코드 함수로 분리

--- client/src/hooks/useHistoryController/index.ts
+++ client/src/hooks/useHistoryController/index.ts
@@ -38,30 +38,28 @@ const useHistoryController = () => {
       setTimeout(() => resolve(func(props)), time);
     });
 
-  const historyController = async ({ fromIdx, toIdx }: IHistoryControllerProps) => {
+  const getFromTo = (fromIdx: number, toIdx: number, isForward: boolean) => {
+    const [forwardIdx, backwardIdx] = isForward ? [fromIdx, toIdx] : [toIdx, fromIdx];
+    return [forwardIdx + 1, backwardIdx + 1 !== 0 ? backwardIdx + 1 : undefined];
+  };
+
+  const restoreAsyncHistory = (stopHistories: IHistoryData[], directionFunc: (props: IHandleMoveProps) => void, fromIdx: number) => {
+    stopHistories.reduce(async (lastPromise, historyData, idx) => {
+      await lastPromise;
+      await wait(700, directionFunc, { historyData, idx, fromIdx: fromIdx });
+    }, Promise.resolve());
+  };
+
+  const historyController = ({ fromIdx, toIdx }: IHistoryControllerProps) => {
     const isForward = fromIdx < toIdx;
+    const [from, to] = getFromTo(fromIdx, toIdx, isForward);
+    const stopHistories = historyDataList.slice(from, to);
 
     if (isForward) {
-      const from = fromIdx + 1;
-      const to = toIdx + 1 !== 0 ? toIdx + 1 : undefined;
-
-      const stopHistories = historyDataList.slice(from, to);
-
-      await stopHistories.reduce(async (lastPromise, historyData, idx) => {
-        await lastPromise;
-        await wait(700, handleMoveForward, { historyData, idx, fromIdx: fromIdx + 1 });
-      }, Promise.resolve());
+      restoreAsyncHistory(stopHistories, handleMoveForward, fromIdx + 1);
     } else {
-      const from = toIdx + 1;
-      const to = fromIdx + 1 !== 0 ? fromIdx + 1 : undefined;
-
-      const stopHistories = historyDataList.slice(from, to);
       stopHistories.reverse();
-
-      await stopHistories.reduce(async (lastPromise, historyData, idx) => {
-        await lastPromise;
-        await wait(700, handleMoveBackward, { historyData, idx, fromIdx: fromIdx - 1 });
-      }, Promise.resolve());
+      restoreAsyncHistory(stopHistories, handleMoveBackward, fromIdx - 1);
     }
   };
 
