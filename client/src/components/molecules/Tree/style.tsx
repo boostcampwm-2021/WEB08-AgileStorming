e9a6f5df65bcde0108deef63c2206802f19d7bcb
SeungHyun Kim <daejjyu@gmail.com>
2021-11-23 00:49:46 +0900

    ♻️ 노드 삭제 style 구현
    NodeDeleteBtn은 의존성이 강해 따로 컴포넌트로 분리하지 않았음

--- client/src/components/molecules/Tree/style.tsx
+++ client/src/components/molecules/Tree/style.tsx
@@ -16,3 +16,16 @@ export const ChildContainer = styled.div`
   ${({ theme }) => theme.flex.column};
   gap: 1rem;
 `;
+
+export const NodeDeleteBtn = styled.div`
+  color: ${(props) => props.theme.color.white};
+  background: blue;
+  font-size: ${(props) => props.theme.fontSize.small};
+  border-radius: 1rem;
+  padding: 0.2rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transform: translate(50%, -50%);
+  cursor: pointer;
+`;
