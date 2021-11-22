5cd246b0bf34097b07687eef3e6070f3c5abc9eb
chosh <chotjd329@hotmail.com>
2021-11-23 00:55:42 +0900

    💄 라벨 리스트 스크롤바 제거 flex wrap 적용
    코드리뷰 반영

--- client/src/components/organisms/LabelList/style.ts
+++ client/src/components/organisms/LabelList/style.ts
@@ -2,10 +2,9 @@ import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
   ${({ theme }) => theme.flex.row}
-  height: 2.1rem;
   margin: 0.2rem 0;
   gap: 0.3rem;
-  overflow-y: hidden;
+  flex-wrap: wrap;
 
   span {
     cursor: pointer;
