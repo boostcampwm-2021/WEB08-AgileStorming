1fa860022258f49a1764772e5b4ff6306482e8be
SeungHyun Kim <daejjyu@gmail.com>
2021-11-25 08:19:58 +0900

    ✨ user focus emit, receive 구현
    기존 함수 수정

--- client/src/pages/Mindmap/index.tsx
+++ client/src/pages/Mindmap/index.tsx
@@ -1,11 +1,18 @@
 import { MindmapTemplate } from 'components/templates';
 import CommonLayout from 'components/templates/CommonLayout';
+import useHistoryEmitter from 'hooks/useHistoryEmitter';
 import { useCallback, useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { selectedNodeIdState } from 'recoil/node';
 
 const MindmapPage = () => {
   const setSelectedNodeId = useSetRecoilState(selectedNodeIdState);
+  const { focusNode } = useHistoryEmitter();
+
+  const selectNode = (nodeId: number | null) => {
+    setSelectedNodeId(nodeId);
+    focusNode(nodeId);
+  };
 
   const HandleNodeClick = useCallback(
     (event: MouseEvent) => {
@@ -13,12 +20,12 @@ const MindmapPage = () => {
       const eventTarget = event.target as HTMLElement;
       const eventParent = eventTarget?.parentNode as HTMLElement;
 
-      if (eventTarget.classList.contains('child-container') || eventTarget.tagName === 'svg') return setSelectedNodeId(null);
-      if (eventParent?.classList.contains('node')) return setSelectedNodeId(Number(eventParent.id));
+      if (eventTarget.classList.contains('child-container') || eventTarget.tagName === 'svg') return selectNode(null);
+      if (eventParent?.classList.contains('node')) return selectNode(Number(eventParent.id));
       if (!eventTarget.classList.contains('mindmap-area')) return;
-      if (eventTarget.classList.contains('node')) return setSelectedNodeId(Number(eventTarget.id));
+      if (eventTarget.classList.contains('node')) return selectNode(Number(eventTarget.id));
 
-      setSelectedNodeId(null);
+      selectNode(null);
     },
     [setSelectedNodeId]
   );
