1a8300748bbbe5f33273ff6a455b467c0bc80c07
SeungHyun Kim <daejjyu@gmail.com>
2021-11-25 06:05:27 +0900

    ✨ Task 완료도 차트 Tooltip 추가 #135
    유저 별 task 수, 평균 처리 수 추가

--- client/src/components/organisms/DoneTaskChart/index.tsx
+++ client/src/components/organisms/DoneTaskChart/index.tsx
@@ -1,7 +1,10 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import useProjectId from 'hooks/useRoomId';
+import { useHistory } from 'react-router';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { doneTaskChartState } from 'recoil/chart';
+import { assigneeFilterState, sprintFilterState } from 'recoil/project';
 
 const DoneTaskChart = () => {
   const doneTaskData = useRecoilValue(doneTaskChartState);
@@ -30,6 +33,26 @@ const DoneTaskChart = () => {
       },
       stackLabels: {
         enabled: true,
+      },
+    },
+
+    tooltip: {
+      formatter: function (this: Highcharts.TooltipFormatterContextObject): string {
+        const sprintName = this.x;
+        const seriesName = this.series.name;
+        const taskNum = this.y;
+        const isSprintTotal = seriesName === '주차 별 할당';
+        const totalTask = isSprintTotal ? series.entire.total : series[seriesName].total;
+        return isSprintTotal
+          ? `
+        <b>${sprintName}</b><br/><br/>
+        전체 TASK 수: ${taskNum}
+        `
+          : `
+        <b>* ${seriesName}</b><br/><br/>
+        TASK 처리 수 : ${taskNum}<br/>
+        평균 처리 수: ${(totalTask / categories.length).toFixed(2)}<br/>
+        `;
       },
     },
 
