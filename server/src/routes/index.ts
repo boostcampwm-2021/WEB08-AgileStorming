84a328849e47d137fdeed1ccd6fbd4bb30da76fe
juyahn <juyahn213@gmail.com>
2021-11-30 17:49:08 +0900

    🔥 sample api 삭제

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,7 +3,6 @@ import authRouter from './auth';
 import userRouter from './user';
 import projectRouter from './project';
 import historyRouter from './history';
-import sampleRouter from './sample';
 
 const router = Router();
 
@@ -11,6 +10,5 @@ router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/project', projectRouter);
 router.use('/history', historyRouter);
-router.use('/sample', sampleRouter);
 
 export default router;
