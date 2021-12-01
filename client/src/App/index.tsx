9046ed1961de7bd1c2d868aeef87130ba3c7d09e
SeungHyun Kim <daejjyu@gmail.com>
2021-12-02 03:16:37 +0900

    ♻️ Header 컴포넌트 App 하위로 위치 변경
    popstate를 제대로 사용하기 위함
    CommonLayout에서는 unmount되서 동작이 이상해진다.

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -6,6 +6,7 @@ import { GlobalModal } from 'components/templates/GlobalModal';
 import { SpinnerBackground, Toast } from 'components/atoms';
 import loadable from '@loadable/component';
 import { TPageComponent } from 'types/page';
+import { Header } from 'components/organisms';
 
 interface IProps {
   page: TPageComponent;
@@ -21,6 +22,7 @@ const App = () => {
     <ThemeProvider theme={common}>
       <RecoilRoot>
         <Global styles={global} />
+        <Header />
         <Switch>
           <Route path='/' exact render={() => <AsyncPage page={'Login'} />} />
           <Route path='/project' render={() => <AsyncPage page={'Project'} />} />
@@ -30,7 +32,6 @@ const App = () => {
           <Route path='/calendar/:projectId' render={() => <AsyncPage page={'Calendar'} />} />
           <Route path='/chart/:projectId' render={() => <AsyncPage page={'Chart'} />} />
           <Route path='/backlog/:projectId' render={() => <AsyncPage page={'Backlog'} />} />
-
           <Redirect from='*' to='/' />
         </Switch>
         <GlobalModal />
